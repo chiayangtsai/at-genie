e88a9d608638d82e518d19c654b3121970d20cda
Chia-Yang Tsai <79623264+chiayangtsai@users.noreply.github.com>
2025-01-22 03:37:34 +0000

    code backup

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ void basic_loop() {
   // index     0, 1, 2 ,3, 4
   int aSize = 5;
 
+  cout << "----- for loop ------ " << endl;
   // for loop usage
   // Q: print out the element value in a in order.
   //    3 6 1 2 7
@@ -40,6 +41,8 @@ void basic_loop() {
     }
     cout << '\n';
   }
+  cout << "----- while loop ------ " << endl;
+  // TBV
 
   // return;  //return can be ignored if the return typis "void"
 }
@@ -72,6 +75,7 @@ void leetcode_find_max() {
 }
 
 void leetcode_shuffle_two_arrays() {
+  printf("--- case 1 ------\n");
   {
     int a[5] = {8, 5, 1, 4, 2};
     int b[5] = {0, 3, 1, 4, 7};
@@ -80,6 +84,18 @@ void leetcode_shuffle_two_arrays() {
 
     int c[10];
     int cSize = 10;
+    for (int i = 0; i < cSize; i++) {
+      int index = i / 2;
+      int index_mod = i % 2;
+      if (index_mod == 0) {
+        c[i] = a[index];
+      } else {
+        c[i] = b[index];
+      }
+    }
+    for (int i = 0; i < cSize; i++) {
+      cout << c[i] << " ";
+    }
 
     // Q: shuffle a and b to c, following index order. print out your results
     //  expected results
@@ -87,29 +103,75 @@ void leetcode_shuffle_two_arrays() {
 
     // HW0108
   }
+
+  printf("--- case 2 ------\n");
+  {
+    int a[8] = {8, 5, 1, 4, 2, 3, 6, 9};
+    int b[4] = {0, 3, 1, 7};
+    int aSize = 8;
+    int bSize = 4;
+
+    int c[12];
+    int cSize = 12;
+    int Size;
+    Size = min(aSize, bSize);
+    int index = 0;
+    for (int i = 0; i < Size; i++) {
+      c[index++] = a[i];
+      c[index++] = b[i];
+    }
+    for (int i = Size; i < aSize; i++) {
+      c[index++] = a[i];
+    }
+    for (int i = Size; i < bSize; i++) {
+      c[index++] = b[i];
+    }
+    for (int i = 0; i < cSize; i++) {
+      cout << c[i] << " ";
+    }
+    // Q: shuffle a and b to c, following index order. print out your results
+    //  expected results
+    //
+    // NOTE: array a and b may have different sizes.
+    // NOTE: you can't assume which array has larger size
+    //
+    //   c[12] will be {8,0,5,3,1,1,4,7,2,3,6,9}
+
+    // HW0108
+  }
 }
 
-void leetcode_bubble_sort(){
-  int a[7]= {3, 7, 1, 0, 9, 2, 3};
+void leetcode_bubble_sort() {
+  int a[7] = {3, 7, 1, 0, 9, 2, 3};
+  int length = sizeof(a) / sizeof(int);
+  for (int i = 0; i < length - 1; i++) {
+    for (int j = 0; j < length - 1 - i; j++) {
+      if (a[j] > a[j + 1])
+        swap(a[j], a[j + 1]);
+    }
+  }
+  for (int i = 0; i < length; i++) {
+    cout << a[i] << " ";
+  }
 
-  //Q: use bubble sort to sort elements in a
+  // Q: use bubble sort to sort elements in a
   //
-  // NOTE: in-pace memory
+  //  NOTE: in-pace memory
   //
 
-  //HW0108
+  // HW0108
 }
 
 int main() {
 
-  int testID = 1;
+  int testID = 5;
 
   switch (testID) {
   case 0:
     std::cout << "Hello World!\n";
 
     break;
-  case 1:
+  case 1: // loop
     basic_loop();
     break;
   case 2:
@@ -129,6 +191,5 @@ int main() {
   // if-else if -else
   // for loop
   // while loop
-  leetcode_find_max();
   return 0;
 }
